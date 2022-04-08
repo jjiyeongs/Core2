3d18d8619d6932975d6ec39cf4b5ea83ac54eef2
jjiyeongs <shinj605@newschool.edu>
2022-04-07 23:55:49 -0400

    new

---
+++ Core 2 Lab/A5/script.js
@@ -0,0 +1,32 @@
+const matrix = document.querySelector(".matrix");
+
+
+function makeID() {
+    let result = "";
+    let characters =
+    "you have reached the end of the internet";
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+    return result;
+}
+  
+function generateText() {
+    const w = 10;
+    const h = 25;
+    for (let i = 0; i < w; i++) {
+        const timeout = Math.floor(Math.random() * 10) * 1000;
+        let delay = 0;
+        const column = document.createElement("p");
+        for (let i = 0; i < h; i++) {
+            const letter = document.createElement("span");
+            letter.innerHTML = makeID();
+            column.appendChild(letter);
+            delay += 100;
+            column.children[i].getElementsByClassName.animationDelay = delay + "ms"
+        }
+        setTimeout(() => {
+            matrix.appendChild(column);
+        }, timeout);
+    }
+}
+
+    generateText();
